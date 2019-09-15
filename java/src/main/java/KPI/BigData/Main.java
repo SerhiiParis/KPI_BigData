edff4f9cc84ae531ed4008b05478d3e71acc1af6
Sergiy Paris <bt1bt1990@gmail.com>
2019-09-15 19:33:57 +0300

    Refactor

--- java/src/main/java/KPI/BigData/Main.java
+++ java/src/main/java/KPI/BigData/Main.java
@@ -4,6 +4,6 @@ import KPI.BigData.WordCount.hadoop.WordCount;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        WordCount.Worker.Start("input_mapreduce.txt", "output_mapreduce.txt");
+        WordCount.Worker.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
     }
 }
