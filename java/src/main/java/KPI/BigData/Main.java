6e5600707b04d1ee1b42c09cd917c5669b0a2675
SergeyParis <bt1bt1990@gmail.com>
2019-10-27 20:30:14 +0300

    Delete unused files

--- java/src/main/java/KPI/BigData/Main.java
+++ java/src/main/java/KPI/BigData/Main.java
@@ -3,9 +3,9 @@ package KPI.BigData;
 public class Main {
     public static void main(String[] args) throws Exception {
         // MapReduce [hadoop]
-        //KPI.BigData.WordCount.hadoop.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
+        KPI.BigData.WordCount.hadoop.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
 
         // MapReduce [spark]
-        KPI.BigData.WordCount.spark.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
+        //KPI.BigData.WordCount.spark.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
     }
 }
