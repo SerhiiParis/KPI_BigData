a691e65b75e8b189cd4c22894fde86112910c681
Sergiy Paris <bt1bt1990@gmail.com>
2019-10-28 15:48:02 +0200

    Revert "Merge branch 'reorganized-version' into master"
    This reverts commit 88c3345c1da0fc98047881b35c7d30ccf51f17ad, reversing
    changes made to 424843cfaccc3f123c94be2d3fa0efc97286557b.

--- java/src/main/java/KPI/BigData/hadoop/WordCount.java
+++ java/src/main/java/KPI/BigData/hadoop/WordCount.java
@@ -12,29 +12,26 @@ import java.util.StringTokenizer;
 
 public class WordCount {
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-        IntWritable number = new IntWritable(1);
-        Text word = new Text();
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
         public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
                 word.set(tokenizer.nextToken());
-                output.collect(word, number);
+                output.collect(word, one);
             }
         }
     }
 
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        private IntWritable result = new IntWritable();
-
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
                 sum += values.next().get();
             }
-            result.set(sum);
-            output.collect(key, result);
+            output.collect(key, new IntWritable(sum));
         }
     }
 
