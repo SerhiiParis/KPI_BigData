9cf4f4b4c9ec87c5186fffbbed2e22c87f609787
SergeyParis <bt1bt1990@gmail.com>
2019-10-28 12:08:24 +0300

    Fix build

--- java/src/main/java/KPI/BigData/Main.java
+++ java/src/main/java/KPI/BigData/Main.java
@@ -3,8 +3,8 @@ package KPI.BigData;
 public class Main {
     public static void main(String[] args) throws Exception {
         // MapReduce [hadoop]
-        //KPI.BigData.hadoop.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
-        KPI.BigData.hadoop.InvertedIndex.Start("inputs/invertedindex_hadoop", "output/invertedindex_hadoop");
+        KPI.BigData.hadoop.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
+        //KPI.BigData.hadoop.InvertedIndex.Start("inputs/invertedindex_hadoop", "output/invertedindex_hadoop");
 
         // MapReduce [spark]
         //KPI.BigData.WordCount.spark.WordCount.Start("inputs/mapreduce_hadoop.txt", "output/mapreduce_hadoop.txt");
--- java/src/main/java/KPI/BigData/hadoop/WordCount.java
+++ java/src/main/java/KPI/BigData/hadoop/WordCount.java
@@ -11,11 +11,11 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class WordCount {
-    public static class Map implements Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
         IntWritable number = new IntWritable(1);
         Text word = new Text();
 
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output) throws IOException {
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
             while (tokenizer.hasMoreTokens()) {
@@ -25,10 +25,10 @@ public class WordCount {
         }
     }
 
-    public static class Reduce implements Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output) throws IOException {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
                 sum += values.next().get();
